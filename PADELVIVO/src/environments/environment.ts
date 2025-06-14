f40736d89cf6c1e1d4a8e97ada84a46bdfefa23b
VanessaObil <vobioso636@g.educaand.es>
2025-06-14 21:02:36 +0200

    http

--- PADELVIVO/src/environments/environment.ts
+++ PADELVIVO/src/environments/environment.ts
@@ -1,12 +1,12 @@
 
-export const environment = {
-production: true,
-apiUrl: 'http://back-padelvivo1-env.eba-evvpm7az.us-east-1.elasticbeanstalk.com'
-};
-
 //export const environment = {
 //production: true,
-//apiUrl: 'http://localhost:8080' // Cambia esto a tu URL de API real
+//apiUrl: 'http://back-padelvivo1-env.eba-evvpm7az.us-east-1.elasticbeanstalk.com'
 //};
+
+export const environment = {
+production: true,
+apiUrl: 'http://localhost:8080' // Cambia esto a tu URL de API real
+};
 
 
